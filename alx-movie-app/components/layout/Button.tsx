1dfaac8c311e9147bf1eaf88fca4053639b0bbac
houssam bouajjioune <houssambouajjioune1998@gmail.com>
2025-07-02 18:18:06 +0100

    Add SVG icons and global styles; configure TypeScript settings
    - Added new SVG icons: file.svg, globe.svg, next.svg, vercel.svg, and window.svg to the public directory.
    - Introduced global CSS styles in globals.css with support for light and dark themes using CSS variables.
    - Configured TypeScript settings in tsconfig.json for better type checking and module resolution.

---
+++ alx-movie-app/components/layout/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LayoutButton = () => {
+  return (
+    <button className="px-6 py-2 bg-green-600 text-white rounded">
+      Layout Action
+    </button>
+  );
+};
+
+export default LayoutButton;
